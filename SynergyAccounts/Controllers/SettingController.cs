5a1f05aa45a3e67d56b9b9860ba9c8353f1cfef9
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-04 11:35:04 +0600

    setting add and branch view

---
+++ SynergyAccounts/Controllers/SettingController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SynergyAccounts.Interface;
+using SynergyAccounts.Models;
+
+namespace SynergyAccounts.Controllers;
+
+public class SettingController : Controller
+{
+    private readonly ICompanyService _companyService;
+    public SettingController(ICompanyService companyService)
+    {
+        _companyService = companyService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Company()
+    {
+        var company = await _companyService.GetCompanyBySubscriptionId();
+        if (company == null)
+        {
+            return View(new Company());
+        }
+        return View(company);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Company(Company company)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(company);
+        }
+
+        try
+        {
+            if (company.Id == 0)
+            {
+                bool isCreated = await _companyService.CreateCompanyAsync(company);
+                if (!isCreated)
+                {
+                    TempData["ErrorMessage"] = "A company already exists for this SubscriptionId.";
+                    return View(company);
+                }
+            }
+            else
+            {
+                bool isUpdated = await _companyService.UpdateCompanyAsync(company);
+                if (!isUpdated)
+                {
+                    TempData["ErrorMessage"] = "Company name already exists or update failed";
+                    return View(company);
+                }
+            }
+            TempData["SuccessMessage"] = "Company Created Successfully";
+            return RedirectToAction("Company");
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError(string.Empty, "An error occurred: " + ex.Message);
+            return View(company);
+        }
+    }
+
+    [HttpGet]
+    public ActionResult Branch()
+    {
+        return View();
+    }
+}
